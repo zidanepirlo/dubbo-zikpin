9840ece9594b01e4da3d72e954ffdfc0cc0bdc3e
yuan <zidanepirlo@163.com>
2018-01-12 17:07:55 +0800

    project init commit

---
+++ src/main/java/com/yuan/dubbo/core/filter/TraceConsumerFilter.java
@@ -0,0 +1,100 @@
+package com.yuan.dubbo.core.filter;
+
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.*;
+import com.google.common.base.Stopwatch;
+import com.twitter.zipkin.gen.Annotation;
+import com.twitter.zipkin.gen.Endpoint;
+import com.twitter.zipkin.gen.Span;
+import com.yuan.dubbo.core.context.TraceContext;
+import com.yuan.dubbo.core.trace.TraceAgent;
+import com.yuan.dubbo.core.utils.IdUtils;
+import com.yuan.dubbo.core.utils.NetworkUtils;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+/*
+* 消费端日志过滤器
+* 作者：姜敏
+* 版本：V1.0
+* 创建日期：2017/4/13
+* 修改日期:2017/4/13
+*/
+@Activate(group = Constants.CONSUMER)
+public class TraceConsumerFilter implements Filter {
+
+    private static final Logger logger = Logger.getLogger(TraceConsumerFilter.class.getName());
+
+    //private TraceAgent traceAgent=new TraceAgent(TraceContext.getTraceConfig().getZipkinUrl());
+
+    private Span startTrace(Invoker<?> invoker, Invocation invocation) {
+
+        Span consumerSpan = new Span();
+
+        Long traceId=null;
+        long id = IdUtils.get();
+        consumerSpan.setId(id);
+        if(null==TraceContext.getTraceId()){
+            TraceContext.start();
+            traceId=id;
+        }
+        else {
+            traceId=TraceContext.getTraceId();
+        }
+
+        consumerSpan.setTrace_id(traceId);
+        consumerSpan.setParent_id(TraceContext.getSpanId());
+        consumerSpan.setName(TraceContext.getTraceConfig().getApplicationName());
+        long timestamp = System.currentTimeMillis()*1000;
+        consumerSpan.setTimestamp(timestamp);
+
+        consumerSpan.addToAnnotations(
+                Annotation.create(timestamp, TraceContext.ANNO_CS,
+                        Endpoint.create(
+                                TraceContext.getTraceConfig().getApplicationName(),
+                                NetworkUtils.ip2Num(NetworkUtils.getSiteIp()),
+                                TraceContext.getTraceConfig().getServerPort() )));
+
+        Map<String, String> attaches = invocation.getAttachments();
+        attaches.put(TraceContext.TRACE_ID_KEY, String.valueOf(consumerSpan.getTrace_id()));
+        attaches.put(TraceContext.SPAN_ID_KEY, String.valueOf(consumerSpan.getId()));
+        return consumerSpan;
+    }
+
+    private void endTrace(Span span, Stopwatch watch) {
+
+        span.addToAnnotations(
+                Annotation.create(System.currentTimeMillis()*1000, TraceContext.ANNO_CR,
+                        Endpoint.create(
+                                span.getName(),
+                                NetworkUtils.ip2Num(NetworkUtils.getSiteIp()),
+                                TraceContext.getTraceConfig().getServerPort())));
+
+        span.setDuration(watch.stop().elapsed(TimeUnit.MICROSECONDS));
+        TraceAgent traceAgent=new TraceAgent(TraceContext.getTraceConfig().getZipkinUrl());
+
+        traceAgent.send(TraceContext.getSpans());
+
+    }
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        if(!TraceContext.getTraceConfig().isEnabled()){
+            return invoker.invoke(invocation);
+        }
+
+        Stopwatch watch = Stopwatch.createStarted();
+        Span span= this.startTrace(invoker,invocation);
+        TraceContext.start();
+        TraceContext.setTraceId(span.getTrace_id());
+        TraceContext.setSpanId(span.getId());
+        TraceContext.addSpan(span);
+        Result result = invoker.invoke(invocation);
+        this.endTrace(span,watch);
+
+        return result;
+    }
+}
---
+++ src/main/java/com/yuan/dubbo/core/filter/TraceProviderFilter.java
@@ -0,0 +1,96 @@
+package com.yuan.dubbo.core.filter;
+
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.*;
+import com.google.common.base.Stopwatch;
+import com.twitter.zipkin.gen.Annotation;
+import com.twitter.zipkin.gen.Endpoint;
+import com.twitter.zipkin.gen.Span;
+import com.yuan.dubbo.core.context.TraceContext;
+import com.yuan.dubbo.core.trace.TraceAgent;
+import com.yuan.dubbo.core.utils.IdUtils;
+import com.yuan.dubbo.core.utils.NetworkUtils;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+/*
+* 服务端日志过滤器
+* 作者：姜敏
+* 版本：V1.0
+* 创建日期：2017/4/13
+* 修改日期:2017/4/13
+*/
+@Activate(group = Constants.PROVIDER)
+public class TraceProviderFilter implements Filter {
+
+    private static final Logger logger = Logger.getLogger(TraceProviderFilter.class.getName());
+
+    private Span startTrace(Map<String, String> attaches) {
+
+        Long traceId = Long.valueOf(attaches.get(TraceContext.TRACE_ID_KEY));
+        Long parentSpanId = Long.valueOf(attaches.get(TraceContext.SPAN_ID_KEY));
+
+        TraceContext.start();
+        TraceContext.setTraceId(traceId);
+        TraceContext.setSpanId(parentSpanId);
+
+        Span providerSpan = new Span();
+
+        long id = IdUtils.get();
+        providerSpan.setId(id);
+        providerSpan.setParent_id(parentSpanId);
+        providerSpan.setTrace_id(traceId);
+        providerSpan.setName(TraceContext.getTraceConfig().getApplicationName());
+        long timestamp = System.currentTimeMillis()*1000;
+        providerSpan.setTimestamp(timestamp);
+
+        providerSpan.addToAnnotations(
+                Annotation.create(timestamp, TraceContext.ANNO_SR,
+                        Endpoint.create(
+                                TraceContext.getTraceConfig().getApplicationName(),
+                                NetworkUtils.ip2Num(NetworkUtils.getSiteIp()),
+                                TraceContext.getTraceConfig().getServerPort() )));
+
+        TraceContext.addSpan(providerSpan);
+        return providerSpan;
+    }
+
+    private void endTrace(Span span, Stopwatch watch) {
+
+        span.addToAnnotations(
+                Annotation.create(System.currentTimeMillis()*1000, TraceContext.ANNO_SS,
+                        Endpoint.create(
+                                span.getName(),
+                                NetworkUtils.ip2Num(NetworkUtils.getSiteIp()),
+                                TraceContext.getTraceConfig().getServerPort())));
+
+        span.setDuration(watch.stop().elapsed(TimeUnit.MICROSECONDS));
+        TraceAgent traceAgent=new TraceAgent(TraceContext.getTraceConfig().getZipkinUrl());
+
+        traceAgent.send(TraceContext.getSpans());
+
+    }
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        if(!TraceContext.getTraceConfig().isEnabled()){
+            return invoker.invoke(invocation);
+        }
+
+        Map<String, String> attaches = invocation.getAttachments();
+        if (!attaches.containsKey(TraceContext.TRACE_ID_KEY)){
+            return invoker.invoke(invocation);
+        }
+        Stopwatch watch = Stopwatch.createStarted();
+        Span providerSpan= this.startTrace(attaches);
+
+        Result result = invoker.invoke(invocation);
+        this.endTrace(providerSpan,watch);
+
+        return result;
+
+    }
+}
